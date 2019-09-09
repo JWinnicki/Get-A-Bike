14fc36e11cb14853d3983c3f3650637b174e3cdf
Jedrzej Winnicki <jwinnicki2@gmail.com>
2019-09-09 22:19:12 +0200

    Updates

---
+++ precache-manifest.65ee2e4f0a9c3d462eb6c3e152aae1eb.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "30c3b19365fb76925798edf7214fe83f",
+    "url": "/Get-A-Bike/static/media/Z125.30c3b193.png"
+  },
+  {
+    "revision": "f34cf8530a426d1e56cc",
+    "url": "/Get-A-Bike/static/css/main.790d674e.chunk.css"
+  },
+  {
+    "revision": "eaaf8ac46a92537b49ad",
+    "url": "/Get-A-Bike/static/js/runtime~main.2b4a33d1.js"
+  },
+  {
+    "revision": "a3fe02e7a12d08f120a8",
+    "url": "/Get-A-Bike/static/js/2.e8f2881e.chunk.js"
+  },
+  {
+    "revision": "9fd78e66aad44a37861b5754fe8b8abe",
+    "url": "/Get-A-Bike/static/media/Logo-second.9fd78e66.png"
+  },
+  {
+    "revision": "144a009cfe43baeb35088aaf63468911",
+    "url": "/Get-A-Bike/static/media/sprite.144a009c.svg"
+  },
+  {
+    "revision": "55383c460b2b28be9657faed136fda5a",
+    "url": "/Get-A-Bike/static/media/Honda-Logo.55383c46.jpg"
+  },
+  {
+    "revision": "b7bd6d101e2fd6c47e8e9b252f09f885",
+    "url": "/Get-A-Bike/static/media/Kawasaki-logo.b7bd6d10.png"
+  },
+  {
+    "revision": "0934e8b4f12dff6a8ba14ce00d58ad82",
+    "url": "/Get-A-Bike/static/media/CB500F.0934e8b4.jpg"
+  },
+  {
+    "revision": "aff97f5e027dacc78eef958f14c0c612",
+    "url": "/Get-A-Bike/static/media/CB650F.aff97f5e.jpg"
+  },
+  {
+    "revision": "1adf5969e057e230b69cd7bf0fe6888f",
+    "url": "/Get-A-Bike/static/media/CB1000R.1adf5969.png"
+  },
+  {
+    "revision": "fe799370a3ba235aeb70bd45d1ab2368",
+    "url": "/Get-A-Bike/static/media/CBR125.fe799370.jpg"
+  },
+  {
+    "revision": "ea5281c1af2d81efe9a5064a41214199",
+    "url": "/Get-A-Bike/static/media/CBR500R.ea5281c1.jpg"
+  },
+  {
+    "revision": "3e1b60ee10d24d04050e85cca8378a83",
+    "url": "/Get-A-Bike/static/media/CBR1000RR.3e1b60ee.jpg"
+  },
+  {
+    "revision": "ed75fc0a840edb63aa3724b918c8606e",
+    "url": "/Get-A-Bike/static/media/kawasaki-ZX6R.ed75fc0a.png"
+  },
+  {
+    "revision": "286478499f80814b31eebbdce720aff8",
+    "url": "/Get-A-Bike/static/media/kawasaki-ZX10R.28647849.jpg"
+  },
+  {
+    "revision": "207a87a4a28f51f6798e5b53a4c896bf",
+    "url": "/Get-A-Bike/static/media/ninja-650.207a87a4.png"
+  },
+  {
+    "revision": "f34cf8530a426d1e56cc",
+    "url": "/Get-A-Bike/static/js/main.fa97fd71.chunk.js"
+  },
+  {
+    "revision": "b3c9efb6d8b8d433a89ed13174f27dcc",
+    "url": "/Get-A-Bike/static/media/Z650.b3c9efb6.png"
+  },
+  {
+    "revision": "382db2c0ae707f1f4348a65acd6bd0e0",
+    "url": "/Get-A-Bike/static/media/Z1000.382db2c0.png"
+  },
+  {
+    "revision": "de923d59bb8e0d10a844e5749d1f2518",
+    "url": "/Get-A-Bike/static/media/GSX-750.de923d59.png"
+  },
+  {
+    "revision": "708f8ad544f91b85dc7e8a7696ec4fca",
+    "url": "/Get-A-Bike/static/media/GSX-1000.708f8ad5.jpg"
+  },
+  {
+    "revision": "d385a9ca0cb9ade76aec7e86dce05e87",
+    "url": "/Get-A-Bike/static/media/GSX-R125.d385a9ca.jpg"
+  },
+  {
+    "revision": "5266080c01366c20341b1451a5af996f",
+    "url": "/Get-A-Bike/static/media/GSX-R1000.5266080c.png"
+  },
+  {
+    "revision": "9f29d3bdf2dda0629b07d9d6d23d12e2",
+    "url": "/Get-A-Bike/static/media/GSX250R.9f29d3bd.jpg"
+  },
+  {
+    "revision": "48d4e3744d03d5e82796372d3ac4570a",
+    "url": "/Get-A-Bike/static/media/SV650.48d4e374.jpg"
+  },
+  {
+    "revision": "82c83fc7b0a01daace5cb6839839f940",
+    "url": "/Get-A-Bike/static/media/MT-07.82c83fc7.jpg"
+  },
+  {
+    "revision": "c139b0542ada1c5ba87b96f31d03defb",
+    "url": "/Get-A-Bike/static/media/MT-10.c139b054.png"
+  },
+  {
+    "revision": "93057f5e13f5a687716b986f6fd2cfcb",
+    "url": "/Get-A-Bike/static/media/MT-125.93057f5e.jpg"
+  },
+  {
+    "revision": "65919f2dcd8aa736b76e6ad0d8760da5",
+    "url": "/Get-A-Bike/static/media/R3.65919f2d.jpg"
+  },
+  {
+    "revision": "1b983e15b1ff2f4e6e6f77b3a4fb7b5b",
+    "url": "/Get-A-Bike/static/media/Yamaha-R1.1b983e15.png"
+  },
+  {
+    "revision": "38b85a29dc06532a64a43bc6380dc716",
+    "url": "/Get-A-Bike/static/media/Yamaha-R6.38b85a29.png"
+  },
+  {
+    "revision": "272eb047e0db9212b1638532f5087348",
+    "url": "/Get-A-Bike/static/media/main.272eb047.jpg"
+  },
+  {
+    "revision": "4f7cd8736d1dada1a9302121d18d978b",
+    "url": "/Get-A-Bike/index.html"
+  }
+];

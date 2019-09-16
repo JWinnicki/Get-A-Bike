50e51058a3bad685713c19aebe4c841459491ead
Jedrzej Winnicki <jwinnicki2@gmail.com>
2019-09-16 15:55:40 +0200

    Footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
             <p className='Footer-text'>Please note that it's not real company website. This is project of fake rental company.</p>
             <div className='Footer-contact'>
                 <p className='Footer-contact--text'>Contact: jwinnicki2@gmail.com</p>
-                <a className='Footer-contact--link' href="https://github.com/JWinnicki" rel="noopener" target="_blank">
+                <a className='Footer-contact--link' href="https://github.com/JWinnicki" rel="noopener noreferrer" target="_blank">
                     <Icon icon='github-sign' size='tiny' color='white' />
                 </a>
-                <a className='Footer-contact--link' href="https://www.linkedin.com/in/j%C4%99drzej-winnicki-a13921193/" rel="noopener" target="_blank">
+                <a className='Footer-contact--link' href="https://www.linkedin.com/in/j%C4%99drzej-winnicki-a13921193/" rel="noopener noreferrer" target="_blank">
                     <Icon  icon='linkedin-logo' size='tiny' color='white' />
                 </a>
             </div>

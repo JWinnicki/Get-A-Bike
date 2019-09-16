0aa400a533f9e5b9b84c1b9879fe08cca6d22a56
Jedrzej Winnicki <jwinnicki2@gmail.com>
2019-09-16 15:53:32 +0200

    Footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import './Footer.css';
+import Icon from '../Icon/Icon';
 
 const Footer = () => {
     return (
         <div className='Footer'>
-            {/* <div className='Footer-items__div'>
-                <div className='Footer-items__item'>About Us</div>
-                <div className='Footer-items__item'>Offer</div>
-                <div className='Footer-items__item'>Contact</div>
-                <div className='Footer-items__item'>Privacy Policy</div>
-                <div className='Footer-items__item'>Careers</div>
+            <p className='Footer-text'>Please note that it's not real company website. This is project of fake rental company.</p>
+            <div className='Footer-contact'>
+                <p className='Footer-contact--text'>Contact: jwinnicki2@gmail.com</p>
+                <a className='Footer-contact--link' href="https://github.com/JWinnicki" rel="noopener" target="_blank">
+                    <Icon icon='github-sign' size='tiny' color='white' />
+                </a>
+                <a className='Footer-contact--link' href="https://www.linkedin.com/in/j%C4%99drzej-winnicki-a13921193/" rel="noopener" target="_blank">
+                    <Icon  icon='linkedin-logo' size='tiny' color='white' />
+                </a>
             </div>
-            <div className='Footer-legal'></div> */}
         </div>
     );
 }
